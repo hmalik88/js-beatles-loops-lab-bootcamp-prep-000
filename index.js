916030665989343ccbf41774a72fda12736cb7ed
Hassan Malik <hbmalik88@gmail.com>
2018-07-28 19:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments) {
 var newlist= [];
-for (let i=0; i<musicians.length+1; i++) {
+for (let i=0; i<musicians.length; i++) {
 newlist[i]=`${musicians[i]} plays ${instruments[i]}`;
 }
 return newlist;
