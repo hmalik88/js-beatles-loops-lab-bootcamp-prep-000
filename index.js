10eda6f3517de27893b650297356b6b18f5b0568
Hassan Malik <hbmalik88@gmail.com>
2018-07-28 19:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay (musicians, instruments) {
 var newlist= [];
 for (let i=0; i<musicians.length+1; i++) {
-newlist[i]=`${musicians[i]} plays ${instruments}`;
+newlist[i]=`${musicians[i]} plays ${instruments[i]}`;
   return newlist;
 }
 }
